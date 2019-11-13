6063bbd85297b6f2cbbddc27e168859e527bec2e
rose-water <rachel.ulgado@gmail.com>
2019-11-12 20:35:58 -0500

    confirmed adafruit sub and pub test ✨🥳

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const PORT    = 3333;
 const app     = express();
 require('dotenv').config();
 
+// -------------------------------------------------------------
 // MQTT client setup
+
+let baseUrl = 'rosewater/feeds/';
+
+// Eventually subscribe to a list of topics, each corresponding
+// to specific devices + MAC addresses?
+let topicsList = [ 'testTopic' ];
+
 const mqttClient = mqtt.connect('mqtts://io.adafruit.com',
   {
     port     : 8883,
@@ -16,8 +24,20 @@ const mqttClient = mqtt.connect('mqtts://io.adafruit.com',
 
 mqttClient.on('connect', () => {
   console.log('MQTT client connected.')
+  mqttClient.subscribe(baseUrl + topicsList[0]);
 });
 
+mqttClient.on('error', (error) => {
+  console.log('MQTT Client Errored');
+  console.log(error);
+});
+
+mqttClient.on('message', (topic, message) => {
+  console.log('msg from topic ' + topic + ': ' + message.toString());
+});
+
+
+// -------------------------------------------------------------
 // EXPRESS server setup
 app.use(express.static(__dirname));
 
